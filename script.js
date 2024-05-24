dc18ea866c45f701b2c2c373204b89d857d2f804
feras10alali <163681534+feras10alali@users.noreply.github.com>
2024-05-24 12:49:09 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 
+  var sentences = ["اختر اللغة.", "choose the language."];
+  function typewriter() {
+  if (i < sentences.length) {
+    var currentSentence = sentences[i]; // Get the current sentence
+
+    // Loop through characters in the current sentence
+    for (var j = 0; j < currentSentence.length; j++) {
+      document.getElementById("typewriter").innerHTML += currentSentence.charAt(j);
+      setTimeout(function() {}, 10); // Short delay between characters
+    }
+
+    // Clear the paragraph after typing the sentence (optional)
+    document.getElementById("typewriter").innerHTML = "";
+
+    i++; // Move to the next sentence in the array
+    setTimeout(typewriter, 1000); // Delay between sentences (1 second here)
+   }
+  }
+
